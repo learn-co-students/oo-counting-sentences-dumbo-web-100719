f775fd894400576b087ea68be5905e96ace38e29
Diego Martinez <dmartinezm@gmail.com>
2019-10-09 13:47:33 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,17 +4,38 @@ class String
 
   def sentence?
     
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
+    if self.end_with?("?") 
+      return true
+    else
+      return false
+    end
 
   end
 
   def exclamation?
-
+    if self.end_with?("!") 
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    # if self.sentence? 
+    # self.split(/[.?!]+/).count  #this regex will just check for (.?!), still works
+    self.split(/[.?!]+(?=\s|\z)/).count # this regex will count for spaces (s) right after (.?!)
+    # end
   end
 end
+
+my_sentence=String.new("This is a sentence! This is another sentence, and another? I don't know what else to type")
+my_sentence2= String.new("This, well, is a sentence. This is too!! And so is this, I think? Woo...")
+binding.pry
