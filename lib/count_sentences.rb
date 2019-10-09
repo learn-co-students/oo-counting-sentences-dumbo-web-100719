b7f8072f7973a60d9244704f4716acc1fd07e1b6
github username <github email address>
2019-10-09 17:31:38 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,36 @@ require 'pry'
 
 class String
 
+  attr_accessor :string
+
   def sentence?
-    
+    if self[-1] == "."
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self[-1] == "?"
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    sentences = 0
+    sentences += self.squeeze.count('.') + self.squeeze.count('?') + self.squeeze.count('!')
+    sentences
+    # binding.pry
   end
 end
