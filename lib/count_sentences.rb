106911bfd7c8c87630d7a4abe9eb153131c0b4f4
Bri Fetterley <bnfetterley@usfca.edu>
2019-10-09 15:25:57 -0400

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,33 @@
+
 require 'pry'
-
 class String
-
-  def sentence?
-    
+  
+    def sentence?
+     if self.end_with?(".")
+      return true 
+     else 
+       return false 
+    end 
+    end
+  
+    def question?
+    if self.end_with?("?")
+      return true 
+     else 
+       return false 
+    end 
+    end
+  
+    def exclamation?
+   if self.end_with?("!")
+      return true 
+     else 
+       return false 
+    end 
+    end
+  
+    def count_sentences
+      #binding.pry
+      words = self.split.select{|word| word.end_with?(".","?","!")}.size
+    end
   end
-
-  def question?
-
-  end
-
-  def exclamation?
-
-  end
-
-  def count_sentences
-
-  end
-end
